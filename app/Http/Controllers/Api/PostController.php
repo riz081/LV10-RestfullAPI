b7bf8286937cf4e9a3a8ea97f3c47beee094bdb2
riz081 <49875729+riz081@users.noreply.github.com>
2023-06-20 23:59:49 +0700

    Restfull API BASIC

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+// Import Model
+use App\Models\Post;
+
+// Import Resource "PostResource"
+use App\Http\Resources\PostResource;
+
+// import facade "Validator"
+use Illuminate\Support\Facades\Validator;
+
+// import facade "Storage"
+use Illuminate\Support\Facades\Storage;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        // get all post
+        $posts = Post::latest()->paginate(5);
+
+        // return collection
+        return new PostResource(status: true, message: 'List Data Posts', resource: $posts);
+    }
+
+    public function store(Request $request){
+        // define validator rules
+        $validator = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:png,jpg,jpeg,gif,svg|max:2048',
+            'title'     => 'required',
+            'content'   => 'required'
+        ]);
+
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        // create post
+        $post = Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content,
+        ]);
+
+        return new PostResource(status: true, message: 'Data Post Berhasil Ditambahkan!', resource: $post);
+    }
+
+    public function show($id){
+
+        // find post by id
+        $post= Post::find($id);
+
+        return new PostResource(status: true, message: 'Detail Data Post!', resource: $post);
+    }
+
+    public function update(Request $request, $id){
+        // rules
+        $validator = Validator::make($request->all(),[
+            'title'     => 'required',
+            'content'   => 'required',
+        ]);
+
+        // check validator
+        if ($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        // find post by id
+        $post = Post::find($id);
+
+        // Check gambar jika tidak kosong
+        if($request->hasFile('image')){
+            // upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // Hapus Gambar Lama
+            Storage::delete('public/posts/'. basename($post->image));
+
+            // ganti gambar / upload gambar baru
+            $post->update([
+                'image'         => $image->hashName(),
+                'title'         => $request->title,
+                'content'       => $request->content,
+            ]);
+        } else {
+            // update post tanpa gambar
+            $post->update([
+                'title'         => $request->title,
+                'content'       => $request->content
+            ]);
+        }
+
+        return new PostResource(status: true, message: 'Data Post Berhasil Diubah!', resource: $post);
+    }
+
+    public function destroy($id){
+        // find post by id
+        $post = Post::find($id);
+
+        // Hapus Gambar
+        Storage::delete('public/posts/'. basename($post->image));
+
+        // delete data
+        $post->delete();
+
+        return new PostResource(true, 'Data Post Berhasil Dihapus!', $post);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// posts
+Route::apiResource(name: '/posts', controller: App\Http\Controllers\Api\PostController::class);
